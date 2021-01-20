35dd3606f0595bfca520918a9d3cb18d40237baf
Rivka Vertsman <rivki058@gmail.com>
2021-01-20 17:10:41 +0200

    sign-in new way

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp (home:HomePage()),);
+import 'login_page.dart';
 
-class HomePage extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-
-  _HomePageState creatState() => _HomePageState();
-}
-
-final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
-
-
-class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
-    Widget build(BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text('google sign-in demo'),),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlatButton(child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('sign-in with Google',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,),),
-                ), onPressed: _signInWithGoogle,
-                  color: Colors.black,
-                )
-              ],
-            ),
-          )
-      );
-    }
-    _signInWithGoogle() async{
-      final GoogleSignInAccount googleUser = await googleSignIn.signIn();
-      //final GoogleSignInAuthenticao
-    }
+    return MaterialApp(
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
   }
-
+}
