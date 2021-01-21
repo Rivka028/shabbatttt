be4b81b418ef3e82b9304f1bbb0c366b53ad62b9
Rivka Vertsman <rivki058@gmail.com>
2021-01-21 17:14:38 +0200

    facebook sign in

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,9 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
+final LoginResult loginResult = LoginResult();
 
+Future<UserCredential> signInWithFacebook() async {
+  // Trigger the sign-in flow
+  final LoginResult result = await FacebookAuth.instance.login();
 
+  // Create a credential from the access token
+  final FacebookAuthCredential facebookAuthCredential =
+  FacebookAuthProvider.credential(result.accessToken.token);
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+}
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
