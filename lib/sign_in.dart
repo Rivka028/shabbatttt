70498d5bf746c076ba7ca14180be552cb0d3b5c8
Esty sicsu <esther6u@gmail.com>
2021-01-24 16:20:42 +0200

    routing main and remote from welcome to login

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -4,19 +4,19 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 
-//final LoginResult loginResult = LoginResult();
+final LoginResult loginResult = LoginResult();
 
-//Future<UserCredential> signInWithFacebook() async {
-  // Trigger the sign-in flow
-  //final LoginResult result = await FacebookAuth.instance.login();
+Future<UserCredential> signInWithFacebook() async {
+ // Trigger the sign-in flow
+  final LoginResult result = await FacebookAuth.instance.login();
 
-  // Create a credential from the access token
-  //final FacebookAuthCredential facebookAuthCredential =
-  //FacebookAuthProvider.credential(result.accessToken.token);
+ // Create a credential from the access token
+  final FacebookAuthCredential facebookAuthCredential =
+  FacebookAuthProvider.credential(result.accessToken.token);
 
   // Once signed in, return the UserCredential
-//  return await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-//}
+  return await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+}
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
